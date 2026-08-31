0fdae6f81038e45919ac797fb9ca25f3101758db
almatov1 <almatovalshin@gmail.com>
2026-08-31 19:16:48 +0500

    B2 completed

---
+++ src/common/constants/topics/b2/index.ts
@@ -0,0 +1,24 @@
+import { Topic } from '..';
+import { B2_1_TOPIC } from './1';
+import { B2_2_TOPIC } from './2';
+import { B2_3_TOPIC } from './3';
+import { B2_4_TOPIC } from './4';
+import { B2_5_TOPIC } from './5';
+import { B2_6_TOPIC } from './6';
+import { B2_7_TOPIC } from './7';
+import { B2_8_TOPIC } from './8';
+import { B2_9_TOPIC } from './9';
+import { B2_10_TOPIC } from './10';
+
+export const B2_TOPICS: Topic[] = [
+    B2_1_TOPIC,
+    B2_2_TOPIC,
+    B2_3_TOPIC,
+    B2_4_TOPIC,
+    B2_5_TOPIC,
+    B2_6_TOPIC,
+    B2_7_TOPIC,
+    B2_8_TOPIC,
+    B2_9_TOPIC,
+    B2_10_TOPIC
+];
--- src/common/constants/topics/index.ts
+++ src/common/constants/topics/index.ts
@@ -2,6 +2,7 @@ import { Level } from "src/generated/prisma/enums";
 import { A1_TOPICS } from "./a1";
 import { A2_TOPICS } from "./a2";
 import { B1_TOPICS } from "./b1";
+import { B2_TOPICS } from "./b2";
 
 export interface Topic {
     title: {
@@ -44,6 +45,6 @@ export const TOPICS: Record<Level, Topic[]> = {
     [Level.A1]: A1_TOPICS,
     [Level.A2]: A2_TOPICS,
     [Level.B1]: B1_TOPICS,
-    [Level.B2]: [],
+    [Level.B2]: B2_TOPICS,
     [Level.C1]: []
 };
